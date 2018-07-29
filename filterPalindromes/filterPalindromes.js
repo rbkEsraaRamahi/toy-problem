6e0331ad0deb558238a1814798ee8e398bc3efca
abdulazeeznaji <abdulazeeznaji@gmail>
2018-07-29 10:59:38 +0300

    filterPalindromes

---
+++ filterPalindromes/filterPalindromes.js
@@ -0,0 +1,15 @@
+//Given an array of words (array of strings), return all words that are palindromes.
+// Example: filterPalindromes([ "word", "Ana", "race car" ])
+// output: [ "Ana", "race car" ]
+
+
+
+
+
+
+
+
+function filterPalindromes (words) {
+  // Write your code here, and
+  // return your final answer.
+}
