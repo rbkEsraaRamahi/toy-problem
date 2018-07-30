892233b961ad1b61a7502ee6414dae0781bd24a2
Belalfaouri <belalfaourig@gmail.com>
2018-07-30 10:29:54 +0300

    binarySearch

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,6 +11,24 @@
  */
 
 var binarySearch = function (array, target) {
-  
-};
+  var startIndex=0
+  var endIndex=array.length-1;
+  var midIndex=Math.floor(array.length/2)
+  var resIndex;
+  function search(startIndex,endIndex,midIndex){
 
+    if(array[midIndex] === target){
+      resIndex=midIndex;
+    }else if (array[midIndex] > target) {
+      console.log('here');
+      endIndex=midIndex;
+      midIndex=Math.floor(midIndex/2)
+      search(startIndex,endIndex,midIndex)
+    }else if (array[midIndex] < target) {
+      startIndex=midIndex
+      midIndex=Math.floor((endIndex-startIndex)/2)
+      search(startIndex,endIndex,midIndex)
+    }
+  }
+  return resIndex
+};
