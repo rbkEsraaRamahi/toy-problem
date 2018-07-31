d5aecb1e4a2ddf8157440875cd6edbd417b192a7
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2018-07-31 09:34:28 +0300

    balancedBrackets

---
+++ balancedBrackets/balancedBrackets.js
@@ -0,0 +1,19 @@
+//Given a string, return true if its arrangement of parenthesis (), 
+//curly-brackets {}, and square-brackets [] would be considered valid in code and mathematics (is balanced).
+//Examples:
+//Input:"(x + y) - (4)"
+//output:true
+//input:"(((10 ) ()) ((?)(:)))"
+//output:true
+//input:"[{()}]"	
+//output:true
+//input:"(50)("
+//output:false
+//input:"[{]}"	
+//output:false
+
+function isBalanced (str) {
+  // Write your code here, and
+  // return your final answer.
+}
+
