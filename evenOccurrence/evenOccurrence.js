9b0d2cd6845a3b2d0253fad904f95134c8747151
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2018-08-05 10:01:32 +0300

    evenOccurrence

---
+++ evenOccurrence/evenOccurrence.js
@@ -0,0 +1,19 @@
+//Find the first item that occurs an even number of times in an array.
+// Remember to handle multiple even-occurrence items and return the first one. 
+//Return null if there are no even-occurrence items.
+//Examples
+
+
+//input:[ 1, 3, 3, 3, 2, 4, 4, 2, 5 ]
+//output:2
+//input:[ "cat", "dog", "dig", "cat" ]
+//output:"cat"
+
+
+
+function evenOccurrence (arr) {
+  // Write your code here, and
+  // return your final answer.
+}
+
+
