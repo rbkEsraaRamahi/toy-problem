f244ce06b1bbf67333a9a91f6596ea90464228ef
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2018-08-09 10:04:01 +0300

    wordsWthinWords

---
+++ wordsWthinWords/wordsWthinWords.js
@@ -0,0 +1,8 @@
+//Words within Words
+//Given an array of unique words, find the word that contains the greatest number of other words. 
+//A word must be at least two letters long.
+//input:
+//wordList:
+//[ "gray", "grays", "ray", "rays", "strays" ]	
+//output:"grays"
+
