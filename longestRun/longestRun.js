ca5a706381c7b5632731eacbca9b5a772403aa59
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2018-08-12 10:05:41 +0300

    longestRun

---
+++ longestRun/longestRun.js
@@ -0,0 +1,28 @@
+/* Write a function that, given a string, 
+finds the longest run of identical characters and 
+returns an array containing the start and end indices of that run. 
+If there are two runs of equal length, return the first one. Return [0,0] for no runs. */
+//Examples:
+
+/* Input	     Output
+  
+  "abbbcc"	     [ 1, 3 ]
+
+  "aabbc"	    [ 0, 1 ]
+
+    ""	        [ 0, 0 ]
+
+"mississippi"   [ 2, 3 ]
+
+"abcdefgh"	    [ 0, 0 ]
+
+"abccccccc"	    [ 2, 8 ]  */
+
+
+function longestRun (string) {
+
+
+
+
+
+}
