7c33df7f5a9098b3972b10bcaa64204a69d0e1f7
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2018-09-02 09:37:58 +0300

    allAnagrams

---
+++ allAnagrams/allAnagrams.js
@@ -0,0 +1,22 @@
+/*
+Given a single input string, write a function that produces all possible
+anagrams of a string and outputs them as an array.
+At first, don’t worry about repeated strings. 
+What time complexity is your solution?
+
+Parameters:
+
+string (required) - a string of characters.
+
+Examples
+
+Input				Output
+string:
+"abc"				[ "abc", "acb", "bac", "bca", "cab", "cba" ]
+
+*/
+
+function allAnagrams (string) {
+  // Write your code here, and
+  // return your final answer.
+}
