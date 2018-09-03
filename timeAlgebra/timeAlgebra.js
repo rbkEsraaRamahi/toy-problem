ad25348b00adc12f48739fe585bc6a53ae573bb3
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2018-09-03 10:12:04 +0300

    timeAlgebra

---
+++ timeAlgebra/timeAlgebra.js
@@ -0,0 +1,31 @@
+/*
+Given a time and an hour amount, return the added result.
+
+Examples
+_____________________________
+Input			Output
+time:
+"3:00pm"
+hours:
+4				"7:00pm"
+____________________________
+time:
+"9:15am"
+hours:
+-2.5			"6:45am"
+____________________________
+time:
+"12:00am"
+hours:
+1				"1:00am"
+_____________________________
+time:
+"1:30pm"
+hours:
+-2				"11:30am"
+______________________________
+*/
+
+function modTime (time, hours) {
+
+}
