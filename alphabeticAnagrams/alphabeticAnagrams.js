28f7bc81d285cc67d519e92d55532a7caf2e463c
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2018-09-05 08:57:35 +0300

    alphabeticAnagrams

---
+++ alphabeticAnagrams/alphabeticAnagrams.js
@@ -0,0 +1,45 @@
+/*
+In an alphabetized array of every anagram of a given string, what index would the given string occupy?
+Consider an anagram as any sequence of letters. For any word with at least two different letters, 
+there are other words composed of the same letters but in a different order (for instance, 
+	“RESISTANCE”/”ANCESTRIES”, which happen to both be dictionary words; for our purposes “SISTANERCE” 
+	is also a word composed of the same letters as these two).
+
+Given a word, return its index number starting at 1 (not 0) to represent the rank of the anagram 
+(for example, the 42nd element is the 42nd alphabetic anagram).
+
+Example for given string ‘cat’:
+
+anagramsOfCat = [‘act’, ‘atc’, ‘cat’, ‘cta’, ‘tac’, ‘tca’];
+
+‘cat’ is the 3rd item in this array, so anagramPosition should return 3; 
+remember that the array is 1-indexed (starts at 1 instead of the usual 0).
+
+Here is the catch: Your function should be able to accept any word 20 letters or less in length 
+(possibly with some letters repeated), and take no more than 5000 milliseconds to run.
+
+Examples
+Input			Output
+string:
+"BAEBEE"			12
+__________________________
+string:
+"ABBS"				1
+__________________________
+string:
+"BABS"				4
+__________________________
+string:
+"ARCTIC"			42
+___________________________
+string:
+"STARK"				92
+____________________________
+
+*/
+
+function anagramPosition (string) {
+  // Write your code here, and
+  // return your final answer.
+}
+
