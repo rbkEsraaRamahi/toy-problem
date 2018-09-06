bdc8b1d8e513de98abe1f6c0c473b8a6bb24975d
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2018-09-06 09:28:52 +0300

    numberToEnglish

---
+++ numberToEnglish/numberToEnglish.js
@@ -0,0 +1,18 @@
+/*
+Write a function numberToEnglish, 
+it should take a number and return the number as a string using English words.
+
+Examples
+Input				Output
+number:
+7					"seven"
+number:
+575				"five hundred seventy-five"
+number:
+78193512		"seventy-eight million one hundred ninety-three thousand five hundred twelve"
+
+*/
+function numberToEnglish (num) {
+  // Write your code here, and
+  // return your final answer.
+}
