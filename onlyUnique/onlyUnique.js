d2a7f6f0331053f6ee3041f2fa45f1781b03c556
Esraa Ramahi <esraaramahi@Esraas-MacBook-Air.local>
2018-09-17 07:24:11 +0300

    onlyUnique

---
+++ onlyUnique/onlyUnique.js
@@ -0,0 +1,23 @@
+/* Given a string, remove any characters that are not unique from the string.
+
+Examples
+------------------------------------
+Input                 |	Output
+------------------------------------
+str:                  |
+"abccdefe"            | "abdf"
+------------------------------------                      
+str:                  | 
+"hello there"         | "o tr"
+------------------------------------                      
+str:                  |
+"xyz"	              | "xyz"
+------------------------------------                      
+str:                  |
+"iiii"                |	""
+------------------------------------
+*/
+
+function onlyUnique (str) {
+   // your code is here
+}	
