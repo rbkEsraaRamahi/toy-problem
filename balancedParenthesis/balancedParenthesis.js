48df8b288b9afe1f276b1463407735d4495f0601
Esraa Ramahi <esraaramahi@Esraas-MacBook-Air.local>
2018-09-19 07:52:33 +0300

    balancedParenthesis

---
+++ balancedParenthesis/balancedParenthesis.js
@@ -0,0 +1,25 @@
+/* Given a string, return true if it contains balanced parenthesis ().
+
+Examples
+----------------------------------
+Input	                 | Output |
+----------------------------------
+string:
+"(x + y) - (4)"	         | true   |
+----------------------------------
+string:
+"(((10 ) ()) ((?)(:)))"	 | true   |
+----------------------------------
+string:
+"(50)("	                 | false  |
+----------------------------------
+string:    
+""	                     | true   |
+----------------------------------
+*/
+
+function isBalanced (string) {
+	// write your code here
+}
+
+
