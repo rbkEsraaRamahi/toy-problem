b334569cb0fa4af21b11bbc7c284640cfe5d0a36
Esraa Ramahi <esraaramahi@Esraas-MacBook-Air.local>
2018-09-20 09:24:07 +0300

    flatten

---
+++ flatten/flatten.js
@@ -0,0 +1,23 @@
+/*
+Given an array of arrays, return all elements in a single array. 
+You must use recursion.
+
+Hint: Use Array.isArray(elem) to see if something is an array.
+
+Examples
+------------------------------------------------------------------
+Input	                                             Output
+arrays:
+[ [ 10 ], [ 20, 30 ], [ 40 ] ]             	   [ 10, 20, 30, 40 ]
+
+arrays:
+[ 1, 2, [ 3 ], 4, [ 5, [ 6 ], 7 ] ]	        [ 1, 2, 3, 4, 5, 6, 7 ]
+
+*/
+
+function flatten (arrays) {
+	// write your code here 
+}
+
+
+
